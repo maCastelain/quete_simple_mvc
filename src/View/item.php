4999f72b039b0d47184ff5daf14e2862a6493f37
Marc-Antoine Castelain <ma.castelain@gmail.com>
2018-10-01 15:21:41 +0200

    Send.

---
+++ src/View/item.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Simple MVC</title>
+
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="icon" href="/assets/images/favicon.png">
+</head>
+
+
+
+<!-- affichage des items récupérés par la requête SQL -->
+<body>
+<section>
+    <h1>Items</h1>
+    <ul>
+        <?php foreach ($items as $item) : ?>
+            <li><?= $item['title'] ?></li>
+        <?php endforeach ?>
+    </ul>
+</section>
+</body>
+</html>
